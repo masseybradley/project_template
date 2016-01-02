ab1cf40d4c37b5d8520157575982d097243e444b
tinytox <massey.bradley@gmail.com>
2016-01-02 19:45:48 +0900

    initial worker heartbeat monitor and django-admin task monitor hook

--- www/veggy_pi/tasks.py
+++ www/veggy_pi/tasks.py
@@ -4,16 +4,39 @@ from exceptions import ImportError, Exception
 from www import settings
 
 
-app = Celery('tasks', broker='amqp://guest@localhost/', backend=settings.CELERY_RESULT_BACKEND)
+app = Celery('tasks', broker=settings.BROKER, backend=settings.CELERY_RESULT_BACKEND)
+
+class VeggyMonitor():
+	def __init__(self, *args, **kwargs):
+		""" 
+		the veggy monitor class implements celery event handlers for 
+		worker-heartbeat
+		"""
+		self.state = app.events.State()
+		self.data = {}
+
+		def get_worker_heartbeat(event):
+			gen = (val for val in self.state.event(event) if val is not None)
+			for val in gen:
+				print val
+				
+			print "STATE_EVENT: %s\nEVENT %s" % (self.state.event(event), event)
+		
+		with app.connection() as connection:
+			recv = app.events.Receiver(connection, handlers={
+				'worker-heartbeat': get_worker_heartbeat,
+	#			'task-sent': get_task_sent,
+			})
+			recv.capture(limit=None, timeout=None, wakeup=True)
+
 
 class VeggyTaskHandler():
 	def __init__(self, *args, **kwargs):
-		self.x = 0
-		self.y = 0
+		pass
 
 	@app.task
-	def add(self):
-		print self.x, self.y
-		total = self.x + self.y
-		print "total: ", total
+	def add(x, y):
+		print x, y
+		total = x + y
+		print 'total: ', total
 		return total
