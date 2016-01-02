53a41d7e2a9debacfc265d8a5d95895b85f5a630
tinytox <massey.bradley@gmail.com>
2016-01-02 22:41:02 +0900

    removed timer models in favor of scheduled tasks

--- www/veggy_pi/models.py
+++ www/veggy_pi/models.py
@@ -10,42 +10,12 @@ from www import settings
 
 
 class RPiPinMap(models.Model):
+	pin = models.SmallIntegerField(blank=False, null=False)
 	label = models.CharField(max_length=50, blank=False, null=False)
-	pin_number = models.SmallIntegerField(blank=False, null=False)
-	is_gpio = models.BooleanField()
 	def __unicode__(self):
-		return "label: %s pin: %s" % (self.label, self.pin_number)
-
-class GenericTimer(models.Model):
-	"""
-	generic timer class to be sublclassed by timer implementations
-	"""
-	start = models.DateTimeField(blank=False, null=False)
-	end = models.DateTimeField(blank=False, null=False)
-	def clean(self):
-		if self.end < self.start:
-			raise ValidationError(_('end time can not be inferieur to start time.'))
+		return "%s - %s" % (self.pin, self.label)
 	class Meta:
-		abstract = True
+		verbose_name = _("RPi Pin Map")
+		unique_together = ('pin', 'label')
 
-class RepeatTimer(GenericTimer):
-	"""
-	repeat timer for elements that need to switch on for a timed duration
-	over a defined period of time.
-	"""
-	on = models.DurationField(blank=False, null=False)
-	off = models.DurationField(blank=False, null=False)
-	def __unicode__(self):
-		return "on: %s - off: %s (between: %s and %s)" % (self.on, self.off, self.start, self.end)
-	def generate_timer(self):
-		# interval = on period + off period
-		# on_period = start + on
-		# off_period = on_period + off
-		interval = self.on + self.off 
-		# total duration = end - start
-		duration = self.end - self.start
-		# total repeat timers = duration / intervals 
-		timers = duration.total_seconds() / interval.total_seconds()
-		print "interval: %s\nduration: %s\non: %s\noff: %s\ntimers: %s" % (interval, duration, self.on, self.off, timers)
-		
 # Create your models here.
