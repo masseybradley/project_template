04e1953e0162fecc50b65ed419adff69af5d6542
tinytox <massey.bradley@gmail.com>
2016-01-03 18:11:20 +0900

    removed launch script log timestamps (annoying) & changed redirection to crush rather than append

--- www/bin/dev.sh
+++ www/bin/dev.sh
@@ -1,24 +1,18 @@
 #!/bin/sh
 
-time_stamp() {
-	TS=`date +%T`
-}
-
 main() {
 	(
-		time_stamp;
-		celerycam.sh >> "$PROJECT_LOG/celery_cam.""$TS"".log" &
+		celerycam.sh > "$PROJECT_LOG/celery_cam.log" &
 	)
 	(
-		time_stamp;
-		runserver_plus.sh >> "$PROJECT_LOG/runserver_plus.""$TS"".log" &
+		runserver_plus.sh > "$PROJECT_LOG/runserver_plus.log" &
 	)
 	(
-		time_stamp;
-		worker.sh >> "$PROJECT_LOG/worker.""$TS"".log" &
+		worker.sh > "$PROJECT_LOG/worker.log" &
 	)
 }
 
 main
 sleep 10
+# enable events
 celery control enable_events
